079fc0739f32cc1c0b8f07d78ef131c2ecc81f8c
zelvarys <me.zelvarys@gmail.com>
2026-09-24 20:15:18 +0100

    chore: remove unused barrel index files

--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  repo: require('./jsonRepository'),
-  users: require('./userModel'),
-  gameStats: require('./gameStatsModel'),
-  botState: require('./botStateModel'),
-  cache: require('./commandCacheModel')
-};
